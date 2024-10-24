a943880f77cfe8669caa02cece1e81f68ca4ef23
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2024-10-24 22:05:25 +0300

    add post method to products list, update gitignore

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Products
 from .serializers import ProductsSerializer
 
@@ -9,4 +10,11 @@ class ProductsView(APIView):
     def get(self, request):
         items = Products.objects.all()
         serializer = ProductsSerializer(items, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
