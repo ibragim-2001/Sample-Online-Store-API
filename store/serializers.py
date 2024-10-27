52fd24bdb30fb819b652ef4db10ebd8c98187878
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2024-10-27 14:56:46 +0300

    update serilizers.py

--- store/serializers.py
+++ store/serializers.py
@@ -4,6 +4,7 @@ from .models import Categories, Products
 
 class ProductsSerializer(serializers.ModelSerializer):
     category = serializers.CharField(source='category.title', read_only=True)
+    # category = serializers.PrimaryKeyRelatedField(queryset=Categories.objects.all())
 
     class Meta:
         model = Products
