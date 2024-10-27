224e7920c205f15a0a33bd6a9535029243e4d2cd
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2024-10-27 23:11:29 +0300

    add pagination in products list

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Products
@@ -8,9 +9,20 @@ from .serializers import ProductsSerializer
 class ProductsView(APIView):
 
     def get(self, request):
-        items = Products.objects.all()
-        serializer = ProductsSerializer(items, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        products = Products.objects.all()
+
+        paginator = PageNumberPagination()
+        paginator.page_size = request.query_params.get('limit', 5)
+        result_page = paginator.paginate_queryset(products, request)
+        
+        serializer = ProductsSerializer(result_page, many=True)
+        
+        return paginator.get_paginated_response(
+            {
+            'data': serializer.data
+            }
+        )
+    
 
     def post(self, request):
         serializer = ProductsSerializer(data=request.data)
