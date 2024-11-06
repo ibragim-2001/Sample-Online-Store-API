7e8a50d98f42a760348ae2b2561cf8544aeb5ff1
ibragim magomedow <ibragimmagomedow@MacBook-Air-ibragim.local>
2024-11-06 21:30:03 +0300

    add cart administration and username searchfield

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 import uuid
 from django.contrib import admin
-from .models import Categories, Products
+from .models import Categories, Products, Cart
 
 
 @admin.register(Categories)
@@ -14,3 +14,8 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
     list_display = ('title', 'price', 'category', 'create_at', 'update_at')
     search_fields = ('title', 'description')
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created')
+    search_fields = ('user__username', )
